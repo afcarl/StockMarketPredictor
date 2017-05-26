e60a4cf6a0edd2aa3abb3b1a9a508ff875c9b9ca
Divyansh Jha <divyanshj.16@gmail.com>
2017-05-26 13:45:16 +0530

    add credentials file

---
+++ twitter_credentials.py
@@ -0,0 +1,10 @@
+credentials = {
+                'ck':'',   # Consumer key
+                'cs':'',   # Consumer secret
+                'at':'',   # access token
+                'ats':''   # access token secret
+}
+
+if __name__ == '__main__':
+    for key in credentials:
+        print (key,credentials[key])
